593ab04fab4b44fb99d12de2346411559ff0a198
Krima Patel <patel.krima@hotmail.com>
2022-07-09 14:00:52 -0500

    worked on state

---
+++ pages/krima.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Loading from '../components/Loading';
+
+export default function krima() {
+  return (
+    <div className="red-text">
+      <Loading />
+      <div>Hello, Krima!</div>
+    </div>
+  );
+}
